97751d7f7552924ca84b66fcb76c8273f876022f
Mansoor <hussainmansoor876@gmail.com>
2019-10-26 12:35:01 +0500

    Done initialization

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Hello JacobpMobile</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
