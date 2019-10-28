944820add080f62b6e9bd6f49fea95eb24abcdf7
Mansoor <hussainmansoor876@gmail.com>
2019-10-28 18:12:41 +0500

    Splash screen done

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './Redux/store';
+import { Splash } from './Screens'
 
 class App extends React.Component {
   constructor() {
@@ -12,11 +13,12 @@ class App extends React.Component {
     return (
       <Provider store={store} >
         <View>
-          <Text>Hello JacobpMobile</Text>
+          <Splash />
         </View>
       </Provider>
     );
   }
 }
+import { from } from 'rxjs';
 
 export default App;
