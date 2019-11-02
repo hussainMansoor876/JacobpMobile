0010f4b52e694a287a0a79542451c2af4143f95f
Mansoor <hussainmansoor876@gmail.com>
2019-11-02 15:20:07 +0500

    Working on Home Screen

--- Screens/Home.js
+++ Screens/Home.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { View, Dimensions, Text, KeyboardAvoidingView, ScrollView, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import SplashScreen from 'react-native-splash-screen'
 import Carousel from 'react-native-snap-carousel';
-import { Button } from 'native-base'
+import { Button, Card, CardItem, Body } from 'native-base'
 import { connect } from 'react-redux';
 import { Icon, Badge } from 'react-native-elements';
 import FeatherIcon from "react-native-feather1s";
+
 
 
 class Home extends React.Component {
@@ -17,22 +18,28 @@ class Home extends React.Component {
             items: {},
             carouselItems: [
                 {
-                    title: "Item 1"
+                    title: "Project Daily stand-up",
+                    time: "10:00 AM - 10:30 AM"
                 },
                 {
-                    title: "Item 2"
+                    title: "Item 2",
+                    time: "10:00 AM - 10:30 AM"
                 },
                 {
-                    title: "Item 3"
+                    title: "Item 3",
+                    time: "10:00 AM - 10:30 AM"
                 },
                 {
-                    title: "Item 4"
+                    title: "Item 4",
+                    time: "10:00 AM - 10:30 AM"
                 },
                 {
-                    title: "Item 5"
+                    title: "Item 5",
+                    time: "10:00 AM - 10:30 AM"
                 }
             ]
         }
+        this._renderItem = this._renderItem.bind(this)
     }
 
     componentDidMount() {
@@ -40,9 +47,27 @@ class Home extends React.Component {
     }
 
     _renderItem({ item, index }) {
+        const { screenWidth } = this.state
         return (
-            <View style={styles.slide}>
-                <Text style={styles.title}>{item.title}</Text>
+            <View style={{ height: 120, width: '100%', justifyContent: 'flex-start', alignItems: 'flex-start', marginTop: 5 }}>
+                <Card style={{ flex: 1, height: 150, width: '90%', flexDirection: 'row', borderRadius: 10 }}>
+                    <CardItem style={{ backgroundColor: '#2DC76D', borderTopLeftRadius: 10, borderBottomLeftRadius: 10, marginRight: -24 }}>
+                    </CardItem>
+                    <CardItem style={{ flex: 1, borderTopRightRadius: 10, borderBottomRightRadius: 10 }}>
+                        <Body>
+                            <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+                                <View style={{ flex: 4 }}>
+                                    <Text style={{ fontSize: 16 }}>{item.title}</Text>
+                                    <Text>{item.time}</Text>
+                                </View>
+                                <View style={{ flex: 1 }}>
+                                    <Image
+                                        source={require('../assets/images/Bitmap1.png')} />
+                                </View>
+                            </View>
+                        </Body>
+                    </CardItem>
+                </Card>
             </View>
         );
     }
@@ -81,13 +106,18 @@ class Home extends React.Component {
                         </Image>
                     </View>
                 </View>
-                <View style={{ paddingTop: 20, justifyContent: 'center', alignItems: 'center', borderWidth: 1, borderColor: 'black' }}>
+                <View style={{ paddingTop: 20, width: screenWidth, justifyContent: 'flex-start', alignItems: 'flex-start', marginLeft: 10, marginTop: 20 }}>
+                    <View>
+                        <Text style={{ fontSize: 24, fontWeight: 'bold'}}>Meetings</Text>
+                    </View>
                     <Carousel
                         ref={(c) => { this._carousel = c; }}
                         data={this.state.carouselItems}
                         renderItem={this._renderItem}
                         sliderWidth={250}
                         itemWidth={250}
+                        loop={true}
+                        enableSnap={true}
                     />
                 </View>
             </View>
