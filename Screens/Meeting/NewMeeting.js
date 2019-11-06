120b1a2ae74ffca44bf824e7db1699434626d45f
Mansoor <hussainmansoor876@gmail.com>
2019-11-06 15:55:05 +0500

    Working on animation

--- Screens/Meeting/NewMeeting.js
+++ Screens/Meeting/NewMeeting.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Dimensions, Text, Image, KeyboardAvoidingView, ScrollView, TouchableOpacity, Platform, SafeAreaView, StyleSheet } from 'react-native';
+import { View, Dimensions, Text, Image, KeyboardAvoidingView, ScrollView, TouchableOpacity, Platform, SafeAreaView, StyleSheet, Animated, Easing } from 'react-native';
 import SplashScreen from 'react-native-splash-screen'
 import { Form, Item, Label, Input, Textarea } from 'native-base'
 import { connect } from 'react-redux';
@@ -7,19 +7,25 @@ import DateTimePicker from "react-native-modal-datetime-picker";
 import { Icon } from 'react-native-elements'
 import FeatherIcon from "react-native-feather1s";
 import Moment from 'moment'
+import { Calendar } from 'react-native-calendars';
+
+const { height, width } = Dimensions.get('window')
 
 
 class NewMeeting extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            screenWidth: Dimensions.get('window').width,
-            screenHeight: Dimensions.get('window').height,
+            screenWidth: width,
+            screenHeight: new Animated.Value(height),
+            screenOpacity: new Animated.Value(1),
             items: {},
             today: 'Today',
             currentTime: Moment(new Date()).format("h:mm A"),
-            futureDate: ''
-
+            futureDate: '',
+            isDateTimePickerVisible: new Animated.Value(0),
+            calendarHeight: new Animated.Value(0),
+            calendarOpacity: new Animated.Value(0)
         }
     }
 
@@ -58,11 +64,37 @@ class NewMeeting extends React.Component {
     }
 
     showDateTimePicker = () => {
-        this.setState({ isDateTimePickerVisible: true });
+        Animated.timing(this.state.screenHeight, {
+            toValue: 0,
+            duration: 500,
+        }).start()
+        Animated.timing(this.state.screenOpacity, {
+            toValue: 0,
+            duration: 500
+        }).start()
+        Animated.timing(this.state.calendarHeight, {
+            // delay: 500,
+            toValue: height,
+            duration: 500
+        }).start()
+        Animated.timing(this.state.calendarOpacity, {
+            // delay: 1000,
+            toValue: 1,
+            duration: 500
+        }).start()
     };
 
+
+
     hideDateTimePicker = () => {
-        this.setState({ isDateTimePickerVisible: false });
+        Animated.timing(this.state.calendarHeight, {
+            toValue: 0,
+            duration: 1000
+        }).start()
+        Animated.timing(this.state.isDateTimePickerVisible, {
+            toValue: 0,
+            duration: 500
+        }).start()
     };
 
     handleDatePicked = date => {
@@ -71,8 +103,7 @@ class NewMeeting extends React.Component {
     };
 
     componentDidMount() {
-        SplashScreen.hide();
-        console.log("Hello", this.state.currentTime)
+        SplashScreen.hide()
     }
 
     onValueChange2(value: string) {
@@ -82,107 +113,123 @@ class NewMeeting extends React.Component {
     }
 
     render() {
-        const { today, currentTime, futureDate, screenHeight, screenWidth } = this.state
+        const { today, currentTime, futureDate, calendarHeight, isDateTimePickerVisible, screenHeight, screenOpacity, calendarOpacity } = this.state
+        console.log(this.state.isDateTimePickerVisible)
         return (
             <SafeAreaView style={{ flex: 1 }}>
-                <ScrollView style={{ width: screenWidth, height: screenHeight }}>
+                <View style={{ flexDirection: 'row', paddingTop: 10, paddingLeft: 10, justifyContent: 'space-between', alignItems: 'center', paddingRight: 10 }}>
+                    <FeatherIcon
+                        name="arrow-left"
+                        size={40}
+                        // thin={true}
+                        // type="font-awesome"
+                        iconStyle={{ width: 200 }}
+                    />
+                </View>
+                {calendarHeight && <Animated.ScrollView style={{ height: 50 }}>
                     <KeyboardAvoidingView
                         resetScrollToCoords={{ x: 0, y: 0 }}
                         behavior={Platform.OS === "ios" ? "padding" : null}
                         keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
                         style={{ flex: 1 }}
                     >
-                        <View style={{ flexDirection: 'row', paddingTop: 10, paddingLeft: 10, justifyContent: 'space-between', alignItems: 'center', paddingRight: 10 }}>
-                            <FeatherIcon
-                                name="arrow-left"
-                                size={40}
-                                // thin={true}
-                                iconStyle={{ width: 200 }}
-                            />
-                        </View>
-                        <View>
-                            <Form>
-                                <Item floatingLabel>
-                                    <Label style={{ fontWeight: '600', fontSize: 22, margin: -8, paddingLeft: 20, opacity: 0.5 }}>Name Your Meeting</Label>
-                                    <Input style={{ paddingLeft: 20 }} />
-                                </Item>
-                            </Form>
-                        </View>
-                        <View style={{ paddingLeft: 30, marginTop: 20 }}>
-                            <Text style={{ fontSize: 20, opacity: 0.6 }}>Invite People</Text>
-                            <View style={{ marginTop: 10 }}>
-                                <TouchableOpacity style={{ borderColor: 'grey', borderStyle: 'dashed', borderWidth: 1, height: 60, width: 60, justifyContent: 'center', alignItems: 'center', borderRadius: 50 }}>
-                                    <Icon
-                                        name="add"
-                                        iconStyle={{ fontSize: 32, opacity: 0.2 }}
-                                    />
-                                </TouchableOpacity>
+                        <Animated.View style={{ height: screenHeight, opacity: screenOpacity }}>
+                            <View>
+                                <Form>
+                                    <Item floatingLabel>
+                                        <Label style={{ fontWeight: '600', fontSize: 22, margin: -8, paddingLeft: 20, opacity: 0.5 }}>Name Your Meeting</Label>
+                                        <Input style={{ paddingLeft: 20 }} />
+                                    </Item>
+                                </Form>
                             </View>
-                            <View style={{ marginTop: 10 }}>
-                                <Text style={{ fontSize: 18, marginBottom: 10, opacity: 0.4 }}>Choose Date</Text>
-                                <TouchableOpacity onPress={this.showDateTimePicker} style={{ flexDirection: 'row' }}>
-                                    <Text style={{ fontSize: 18, marginRight: 30 }}>{today}, {currentTime} --- {futureDate}</Text>
-                                    <Icon
-                                        name="angle-down"
-                                        type="font-awesome"
-                                    />
-                                </TouchableOpacity>
-                                <DateTimePicker
-                                    isVisible={this.state.isDateTimePickerVisible}
-                                    onConfirm={this.handleDatePicked}
-                                    onCancel={this.hideDateTimePicker}
-                                    date={new Date()}
-                                    mode="datetime"
-                                    minimumDate={new Date()}
-                                />
-                            </View>
-                            <View style={{ marginTop: 20 }}>
-                                <Text style={{ fontSize: 18, marginBottom: 10, opacity: 0.5 }}>Choose Meeting Type</Text>
-                                <View style={{ marginTop: 5, flexDirection: 'row', marginRight: 20 }}>
-                                    <TouchableOpacity style={{ flexDirection: 'row', backgroundColor: '#E9F9F0', height: 30, paddingTop: 2, borderRadius: 15, width: '45%', marginRight: 15 }}>
-                                        <Image
-                                            style={{ marginRight: 10 }}
-                                            source={require('../../assets/images/Bitmap1.png')} />
-                                        <Text style={{ color: '#22D369', fontSize: 16, fontWeight: '700' }}>Project Meeting</Text>
-                                    </TouchableOpacity>
-                                    <TouchableOpacity style={{ flexDirection: 'row', backgroundColor: '#DFDFF7', height: 30, paddingTop: 2, borderRadius: 15, width: '35%' }}>
-                                        <Image
-                                            style={{ marginRight: 10 }}
-                                            source={require('../../assets/images/message.png')} />
-                                        <Text style={{ color: '#2F61D5', fontSize: 16, fontWeight: '700' }}>Meeting</Text>
+                            <View style={{ paddingLeft: 30, marginTop: 20 }}>
+                                <Text style={{ fontSize: 20, opacity: 0.6 }}>Invite People</Text>
+                                <View style={{ marginTop: 10 }}>
+                                    <TouchableOpacity activeOpacity={0.6} style={{ borderColor: 'grey', borderStyle: 'dashed', borderWidth: 1, height: 60, width: 60, justifyContent: 'center', alignItems: 'center', borderRadius: 50 }}>
+                                        <Icon
+                                            name="add"
+                                            iconStyle={{ fontSize: 32, opacity: 0.2 }}
+                                        />
                                     </TouchableOpacity>
                                 </View>
-                                <View style={{ marginTop: 5, flexDirection: 'row', marginRight: 20 }}>
-                                    <TouchableOpacity style={{ flexDirection: 'row', backgroundColor: '#FFE2DC', height: 30, paddingTop: 2, borderRadius: 15, marginRight: 15, width: '30%' }}>
-                                        <Image
-                                            style={{ marginRight: 10 }}
-                                            source={require('../../assets/images/webinar.png')} />
-                                        <Text style={{ color: '#FF7052', fontSize: 16, fontWeight: '700' }}>Webinar</Text>
+                                <Animated.View style={{ marginTop: 10 }}>
+                                    <Text style={{ fontSize: 18, marginBottom: 10, opacity: 0.4 }}>Choose Date</Text>
+                                    <TouchableOpacity onPress={this.showDateTimePicker} style={{ flexDirection: 'row' }}>
+                                        <Text style={{ fontSize: 18, marginRight: 30 }}>{today}, {currentTime} --- {futureDate}</Text>
+                                        <Icon
+                                            name="angle-down"
+                                            type="font-awesome"
+                                        />
                                     </TouchableOpacity>
-                                    <TouchableOpacity style={{ flexDirection: 'row', backgroundColor: '#E3D9FC', height: 30, paddingTop: 2, borderRadius: 15, marginRight: 15, width: '30%' }}>
-                                        <Image
-                                            style={{ marginRight: 10 }}
-                                            source={require('../../assets/images/status.png')} />
-                                        <Text style={{ color: '#7540EE', fontSize: 16, fontWeight: '700' }}>Status</Text>
-                                    </TouchableOpacity>
-                                    <TouchableOpacity style={{ flexDirection: 'row', backgroundColor: '#D8F0F8', height: 30, paddingTop: 2, borderRadius: 15, marginRight: 15, width: '30%' }}>
-                                        <Image
-                                            style={{ marginRight: 10 }}
-                                            source={require('../../assets/images/other.png')} />
-                                        <Text style={{ color: '#3FB6DC', fontSize: 16, fontWeight: '700' }}>Other</Text>
-                                    </TouchableOpacity>
+                                </Animated.View>
+                                <View style={{ marginTop: 20 }}>
+                                    <Text style={{ fontSize: 18, marginBottom: 10, opacity: 0.5 }}>Choose Meeting Type</Text>
+                                    <View style={{ marginTop: 5, flexDirection: 'row', marginRight: 20 }}>
+                                        <TouchableOpacity style={{ flexDirection: 'row', backgroundColor: '#E9F9F0', height: 30, paddingTop: 2, borderRadius: 15, width: '45%', marginRight: 15 }}>
+                                            <Image
+                                                style={{ marginRight: 10 }}
+                                                source={require('../../assets/images/Bitmap1.png')} />
+                                            <Text style={{ color: '#22D369', fontSize: 16, fontWeight: '700' }}>Project Meeting</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity style={{ flexDirection: 'row', backgroundColor: '#DFDFF7', height: 30, paddingTop: 2, borderRadius: 15, width: '35%' }}>
+                                            <Image
+                                                style={{ marginRight: 10 }}
+                                                source={require('../../assets/images/message.png')} />
+                                            <Text style={{ color: '#2F61D5', fontSize: 16, fontWeight: '700' }}>Meeting</Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                    <View style={{ marginTop: 5, flexDirection: 'row', marginRight: 20 }}>
+                                        <TouchableOpacity style={{ flexDirection: 'row', backgroundColor: '#FFE2DC', height: 30, paddingTop: 2, borderRadius: 15, marginRight: 15, width: '30%' }}>
+                                            <Image
+                                                style={{ marginRight: 10 }}
+                                                source={require('../../assets/images/webinar.png')} />
+                                            <Text style={{ color: '#FF7052', fontSize: 16, fontWeight: '700' }}>Webinar</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity style={{ flexDirection: 'row', backgroundColor: '#E3D9FC', height: 30, paddingTop: 2, borderRadius: 15, marginRight: 15, width: '30%' }}>
+                                            <Image
+                                                style={{ marginRight: 10 }}
+                                                source={require('../../assets/images/status.png')} />
+                                            <Text style={{ color: '#7540EE', fontSize: 16, fontWeight: '700' }}>Status</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity style={{ flexDirection: 'row', backgroundColor: '#D8F0F8', height: 30, paddingTop: 2, borderRadius: 15, marginRight: 15, width: '30%' }}>
+                                            <Image
+                                                style={{ marginRight: 10 }}
+                                                source={require('../../assets/images/other.png')} />
+                                            <Text style={{ color: '#3FB6DC', fontSize: 16, fontWeight: '700' }}>Other</Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>
+                                <View style={{ marginTop: 20, marginRight: 20 }}>
+                                    <Label style={{ marginLeft: 3 }}>Write short Description</Label>
+                                    <Textarea rowSpan={4}
+                                        style={{ borderWidth: 1, borderColor: 'white', borderBottomColor: 'rgba(0,0,0,0.3)', backgroundColor: 'rgba(0,0,0,0.009)' }}
+                                        placeholder="Message..." />
+                                    <Text style={{ color: 'rgba(0,0,0,0.5)', fontSize: 18, fontWeight: 'bold', marginTop: 15, marginLeft: 30 }}>Schedule meeting</Text>
                                 </View>
                             </View>
-                            <View style={{ marginTop: 20, marginRight: 20 }}>
-                                <Label style={{ marginLeft: 3 }}>Write short Description</Label>
-                                <Textarea rowSpan={4}
-                                    style={{ borderWidth: 1, borderColor: 'white', borderBottomColor: 'rgba(0,0,0,0.3)', backgroundColor: 'rgba(0,0,0,0.009)' }}
-                                    placeholder="Message..." />
-                                <Text style={{ color: 'rgba(0,0,0,0.5)', fontSize: 18, fontWeight: 'bold', marginTop: 15, marginLeft: 30 }}>Schedule meeting</Text>
-                            </View>
-                        </View>
+                        </Animated.View>
                     </KeyboardAvoidingView>
-                </ScrollView>
+                </Animated.ScrollView>}
+
+                <Animated.View style={{ marginTop: 10, height: calendarHeight, opacity: calendarOpacity }}>
+                    <View style={{ flexDirection: 'row', paddingLeft: 20, justifyContent: 'space-between', paddingRight: 20 }}>
+                        <Text style={{ fontSize: 18, marginBottom: 10, opacity: 0.4 }}>Choose Date</Text>
+                        <Icon
+                            name="angle-down"
+                            type="font-awesome"
+                        />
+                    </View>
+                    <Calendar
+                        // Collection of dates that have to be marked. Default = {}
+                        onDayPress={(e) => console.log(e)}
+                        markedDates={{
+                            '2012-05-16': { selected: true, marked: true, selectedColor: 'blue' },
+                            '2012-05-17': { marked: true },
+                            '2012-05-18': { marked: true, dotColor: 'red', activeOpacity: 0 },
+                            '2012-05-19': { disabled: true, disableTouchEvent: true }
+                        }}
+                    />
+                </Animated.View>
             </SafeAreaView>
         );
     }
