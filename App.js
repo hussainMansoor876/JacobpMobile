70345b466e3343302060f07544ebd5d973ea6249
Mansoor <hussainmansoor876@gmail.com>
2019-11-08 22:42:05 +0500

    Falcon Done

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import store from './Redux/store';
 import Navigator from './navigation/AppNavigator'
 import CalendarMeeting from './Screens/CalendarMeeting'
-import NewMeeting from './Screens/Meeting/NewMeeting'
+import NewMeeting from './Screens/Meeting/Falcon'
 
 class App extends React.Component {
   constructor() {
---
+++ Screens/Meeting/Falcon.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Dimensions, Image, Text, TouchableOpacity } from 'react-native';
+import SplashScreen from 'react-native-splash-screen'
+import { Icon } from 'react-native-elements'
+
+class Falcon extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            screenWidth: Dimensions.get('window').width,
+            screenHeight: Dimensions.get('window').height
+        }
+    }
+
+    componentDidMount() {
+        SplashScreen.hide();
+    }
+
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+                <View style={{ flexDirection: 'row', padding: 20, justifyContent: 'space-between' }}>
+                    <Text style={{ fontSize: 24 }}>Falcon</Text>
+                    <TouchableOpacity>
+                        <Icon
+                            type="feather"
+                            name="x"
+                        />
+                    </TouchableOpacity>
+                </View>
+                <Image
+                    style={{ marginBottom: 10 }}
+                    source={require('../../assets/images/falcon.png')}
+                />
+                <Text style={{ fontSize: 20, textAlign: 'center' }}>
+                    We couldn’t find {'\n'} what you were looking for.
+                </Text>
+                <Text style={{ fontSize: 16, textAlign: 'center' }}>Sorry :(</Text>
+            </View>
+        );
+    }
+}
+
+export default Falcon;
