c9b8eede7c8dbcfbebd8cafdffa2ecebd5d43b61
Mansoor <hussainmansoor876@gmail.com>
2019-11-10 20:28:51 +0500

    darka nd light mode done on chat

--- Screens/FacebookChat/ChatList.js
+++ Screens/FacebookChat/ChatList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Image, Dimensions } from 'react-native'
+import { Image, Dimensions, SafeAreaView, ScrollView } from 'react-native'
 import { Container, Header, Content, List, ListItem, Left, Body, Right, Thumbnail, Text } from 'native-base';
 import ToggleSwitch from 'toggle-switch-react-native'
 
@@ -11,49 +11,109 @@ export default class ChatList extends Component {
 
         this.state = {
             on: false,
-            label: "Dark Mode"
+            label: "Dark Mode",
+            chatColor: 'black',
+            chatBackground: '#fff',
+            list: [
+                {
+                    title: 'Amy Farha',
+                    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+                    subtitle: 'Doing what you like will always keep you happy . .'
+                },
+                {
+                    title: 'Chris Jackson',
+                    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+                    subtitle: 'Doing what you like will always keep you happy . .'
+                },
+                {
+                    title: 'Farha',
+                    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+                    subtitle: 'Doing what you like will always keep you happy . .'
+                },
+                {
+                    title: 'Chris Jackson',
+                    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+                    subtitle: 'Doing what you like will always keep you happy . .'
+                },
+                {
+                    title: 'Amy',
+                    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+                    subtitle: 'Doing what you like will always keep you happy . .'
+                },
+                {
+                    title: 'Chris',
+                    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+                    subtitle: 'Doing what you like will always keep you happy . .'
+                },
+                {
+                    title: 'Amy Jackson',
+                    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+                    subtitle: 'Doing what you like will always keep you happy . .'
+                },
+                {
+                    title: 'Jackson Jackson',
+                    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+                    subtitle: 'Doing what you like will always keep you happy . .'
+                },
+                {
+                    title: 'Jackson Farha',
+                    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+                    subtitle: 'Doing what you like will always keep you happy . .'
+                },
+                {
+                    title: 'Chris Farha',
+                    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+                    subtitle: 'Doing what you like will always keep you happy . .'
+                },
+            ]
         }
     }
 
 
     render() {
-        const { on } = this.state
+        const { on, list, chatColor, chatBackground } = this.state
         return (
-            <Container>
-                <Header style={{ backgroundColor: '#fff' }}>
+            <SafeAreaView style={{ flex: 1, backgroundColor: chatBackground }}>
+                <Header style={{ backgroundColor: chatBackground, borderBottomWidth: 0 }}>
                     <Body style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
                         <Image
                             source={require('../../assets/images/left.png')}
                         />
-                        <Text style={{ fontSize: 24, marginLeft: -20, textAlign: 'center' }}>Messages</Text>
+                        <Text style={{ fontSize: 24, marginLeft: -20, textAlign: 'center', color: chatColor }}>Messages</Text>
                         <ToggleSwitch
                             isOn={on}
-                            onColor="rgba(0,0,0,0.5)"
+                            onColor="rgba(255,255,255,0.5)"
                             offColor="rgba(0,0,0,0.1)"
                             // label="Switch to Dark Mode"
                             // labelStyle={{ color: "black", fontWeight: "900" }}
                             size="medium"
-                            onToggle={isOn => this.setState({ on: isOn })}
+                            onToggle={isOn => this.setState({ on: isOn, chatColor: isOn ? '#fff' : 'black', chatBackground: isOn ? 'black' : '#fff' })}
                         />
                     </Body>
                 </Header>
-                <Content>
-                    <List>
-                        <ListItem avatar>
-                            <Left>
-                                <Thumbnail source={{ uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg' }} />
-                            </Left>
-                            <Body>
-                                <Text>Kumar Pratik</Text>
-                                <Text note>Doing what you like will always keep you happy . .</Text>
-                            </Body>
-                            <Right>
-                                <Text note>3:43 pm</Text>
-                            </Right>
-                        </ListItem>
+                <ScrollView>
+                    <List style={{ borderTopColor: 'white', borderTopWidth: 0.3 }}>
+                        {
+                            list.map((v, i) => {
+                                return (
+                                    <ListItem avatar key={i}>
+                                        <Left>
+                                            <Thumbnail source={{ uri: v.avatar_url }} />
+                                        </Left>
+                                        <Body>
+                                            <Text style={{ color: chatColor }}>{v.title}</Text>
+                                            <Text note style={{ color: chatColor }}>{v.subtitle}</Text>
+                                        </Body>
+                                        <Right>
+                                            <Text note>3:43 pm</Text>
+                                        </Right>
+                                    </ListItem>
+                                )
+                            })
+                        }
                     </List>
-                </Content>
-            </Container>
+                </ScrollView>
+            </SafeAreaView>
         );
     }
 }
