8553145583dca56cccc36d66b70ee68ba119bad4
Mansoor <hussainmansoor876@gmail.com>
2019-11-22 21:56:09 +0500

    Navbar complete

--- Screens/SideBar/MainSiderBar.js
+++ Screens/SideBar/MainSiderBar.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Image, TouchableOpacity, View, SafeAreaView, ScrollView, StyleSheet } from "react-native";
 import { DrawerActions } from 'react-navigation-drawer';
-import { Icon as ReactIcons } from 'react-native-elements'
+import LinearGradient from 'react-native-linear-gradient';
 import { connect } from 'react-redux'
 import {
   Content,
@@ -11,9 +11,8 @@ import {
   Icon,
   Container,
   Left,
-  Right,
-  Badge,
-  CheckBox
+  Item,
+  Input
 } from "native-base";
 import styles from "./style";
 import * as Animatable from 'react-native-animatable';
@@ -37,61 +36,16 @@ class MainSideBar extends Component {
         {
           name: "Contacts",
           route: "Instagram",
-          icon: "contacts",
-          bg: "#F0B601",
+          icon: "wechat",
+          bg: "#4DCAE0",
           types: "4",
-          image: require('../../assets/images/day3.png')
-
         },
         {
-          name: "Facebook",
-          route: "Facebook",
-          icon: "facebook",
-          bg: "#3b579d",
-          image: require("../../assets/images/schedule.png")
-        },
-        {
-          name: "IMessage",
+          name: "Text Messages",
           route: "IMessage",
           icon: "comment",
           bg: "#477EEA",
           types: "11",
-          image: require('../../assets/images/day.png')
-
-        },
-        {
-          name: "Instagram",
-          route: "Instagram",
-          icon: "instagram",
-          bg: "#F0B601",
-          types: "4",
-          image: require('../../assets/images/day3.png')
-
-        },
-        {
-          name: "Team",
-          route: "Team",
-          icon: "comments",
-          bg: "#C5F442",
-          types: "5",
-          image: require('../../assets/images/week.png')
-
-        },
-        {
-          name: "Twitter",
-          route: "Twitter",
-          icon: "twitter",
-          bg: "#1c9deb",
-          image: require('../../assets/images/minus.png')
-
-        },
-        {
-          name: "Website",
-          route: "Website",
-          icon: "commenting",
-          bg: "#4DCAE0",
-          image: require('../../assets/images/minus.png')
-
         },
         {
           name: "WhatsApp",
@@ -99,127 +53,104 @@ class MainSideBar extends Component {
           icon: "whatsapp",
           bg: "#3FBB25",
           types: "9",
-          image: require('../../assets/images/minus.png')
         },
-      ],
-      applicationList: [
         {
-          name: "Mail",
+          name: "Facebook",
           route: "Facebook",
-          icon: "gmail",
+          icon: "facebook",
           bg: "#3b579d",
-          image: require("../../assets/images/schedule.png")
         },
         {
-          name: "Calendar",
-          route: "Team",
-          icon: "calendar-text",
-          bg: "#C5F442",
-          types: "5",
-          image: require('../../assets/images/week.png')
+          name: "Instagram",
+          route: "Instagram",
+          icon: "instagram",
+          bg: "#F0B601",
+          types: "4",
 
         },
         {
-          name: "File Manager",
-          route: "WhatsApp",
-          icon: "file-document-edit",
-          bg: "#3FBB25",
-          types: "9",
-          image: require('../../assets/images/minus.png')
+          name: "Twitter",
+          route: "Twitter",
+          icon: "twitter",
+          bg: "#1c9deb",
         },
+        {
+          name: "Website Chat",
+          route: "Website",
+          icon: "commenting",
+          bg: "#4DCAE0",
+        },
+        {
+          name: "Team Discussions",
+          route: "Team",
+          icon: "comments",
+          bg: "#C5F442",
+          types: "5",
+        }
       ]
     }
   }
 
   render() {
-    const { listData, applicationList } = this.state
-    const { bool } = this.state
+    const { listData } = this.state
     return (
       <SafeAreaView style={{ flex: 1 }}>
         <Container>
           <Content
             bounces={false}
-            style={{ flex: 1, backgroundColor: '#645AFF', top: -1 }}
+            style={{ flex: 1, top: -1, backgroundColor: '#6A5CFF' }}
           >
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-              {/* <Text style={{ padding: 15, fontSize: 22, fontWeight: '700', color: bool ? '#fff' : 'black' }}>
-                dash
-                <Text style={{ fontSize: 22, fontWeight: '100', color: 'blue' }}>
-                  forge
-                </Text>
-              </Text> */}
-              <ReactIcons
+            <LinearGradient colors={['#9C6FFF', '#6A5CFF']}>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between', margin: 20 }}>
+                <Item style={{ flex: 1, backgroundColor: '#9F7CFF', borderRadius: 50, paddingRight: 10, paddingLeft: 10 }}>
+                  <Input placeholder="Type a message" placeholderTextColor="#fff" />
+                  <AnimatedIcon
+                    name='search'
+                    color='#fff'
+                    size={22}
+                    style={styles.icon}
+                  />
+                </Item>
+                {/* <ReactIcons
                 onPress={() => this.props.navigation.dispatch(DrawerActions.closeDrawer())}
                 containerStyle={{ padding: 15 }}
                 name="x"
                 color='#fff'
                 type="feather"
-              />
-            </View>
-            {/* <Text style={{ marginLeft: 22, fontSize: 20, color: '#fff' }}>APPLICATIONS</Text> */}
-            <List>
-              {
-                applicationList.map((v, i) => {
-                  return (
-                    <ListItem
-                      button
-                      noBorder
-                      onPress={() => this.props.navigation.navigate(v.route)}
-                      key={i}
-                    >
-                      <Left>
-                        <AnimatedMaterial
-                          name={v.icon}
-                          color={v.bg}
-                          size={22}
-                          style={styles.icon}
-                        />
-                        <Text style={{
-                          fontWeight: Platform.OS === "ios" ? "500" : "400",
-                          fontSize: 16,
-                          marginLeft: 20,
-                          color: '#fff'
-                        }}>
-                          {v.name}
-                        </Text>
-                      </Left>
-                    </ListItem>
-                  )
-                })
-              }
-            </List>
-            {/* <Text style={{ marginLeft: 22, fontSize: 20, color: '#fff' }}>CHATS</Text> */}
-            <List>
-              {
-                listData.map((v, i) => {
-                  return (
-                    <ListItem
-                      button
-                      noBorder
-                      onPress={() => this.props.navigation.navigate(v.route)}
-                      key={i}
-                    >
-                      <Left>
-                        <AnimatedIcon
-                          name={v.icon}
-                          color={v.bg}
-                          size={22}
-                          style={styles.icon}
-                        />
-                        <Text style={{
-                          fontWeight: Platform.OS === "ios" ? "500" : "400",
-                          fontSize: 16,
-                          marginLeft: 20,
-                          color: '#fff'
-                        }}>
-                          {v.name}
-                        </Text>
-                      </Left>
-                    </ListItem>
-                  )
-                })
-              }
-            </List>
+              /> */}
+              </View>
+              <List>
+                {
+                  listData.map((v, i) => {
+                    return (
+                      <ListItem
+                        button
+                        noBorder
+                        onPress={() => this.props.navigation.navigate(v.route)}
+                        key={i}
+                      >
+                        <Left>
+                          <AnimatedIcon
+                            name={v.icon}
+                            color={v.bg}
+                            size={22}
+                            style={styles.icon}
+                          />
+                          <Text style={{
+                            fontWeight: Platform.OS === "ios" ? "500" : "400",
+                            fontSize: 16,
+                            marginLeft: 20,
+                            color: '#fff'
+                          }}>
+                            {v.name.toLocaleUpperCase()}
+                          </Text>
+                        </Left>
+                      </ListItem>
+                    )
+                  })
+                }
+              </List>
+            </LinearGradient>
           </Content>
         </Container>
       </SafeAreaView>
